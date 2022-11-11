3b85eca3074af3e08263f90d35d7594c3c27d38b
Remi <remigiusz.terek@wp.pl>
2022-11-11 11:59:02 +0100

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header_container">
         <h5>Hello I'm</h5>
         <h1>Remigiusz Terek</h1>
-        <h5 className="text-light">Front-end Developer</h5>
+        <h5 className="text-light">Full-Stack Developer</h5>
         <CTA />
         <HeaderSocial />
 
