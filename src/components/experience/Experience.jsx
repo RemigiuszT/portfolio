d865f05be3b8a57271818f1b0d1d67f54e131f85
Remi <remigiusz.terek@wp.pl>
2023-01-05 17:38:37 +0100

    +PHP

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -64,7 +64,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>MySQL</h4>
+                <h4>PHP</h4>
                 <small className="text-light">Trainee</small>
               </div>
             </article>
